173693c04444e13706f93e3cc11f4a83cec91328
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-08-27 04:50:50 +0100

    Add total counter

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-let count = 0
+let totalEl = document.getElementById("total-el")
+let count = 0;
+let totalCount = 0;
+totalEl.textContent = totalCount;
 
 function increment() {
     count += 1
@@ -10,6 +13,9 @@ function increment() {
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
+    totalCount += count
+    totalEl.textContent = totalCount
+
     countEl.textContent = 0
     count = 0
 }
